646c08180186c9ef3500a8345c7708560a0ff8d1
tanraya <demerest@gmail.com>
2013-07-28 02:01:14 +0600

    Update config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,31 +1,19 @@
-deploy_to   = "/home/petrovich_demo"
-rails_root  = "#{deploy_to}/current"
-pid_file    = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file = "#{deploy_to}/shared/sockets/unicorn.sock"
-log_file    = "#{rails_root}/log/unicorn.log"
-err_log     = "#{rails_root}/log/unicorn_error.log"
-old_pid     = pid_file + '.oldbin'
+# set path to app that will be used to configure unicorn, 
+# note the trailing slash in this example
+@dir = "/home/petrovich_demo/current"
 
-timeout          30
 worker_processes 2
-listen           socket_file, :backlog => 1024
-pid              pid_file
-stderr_path      err_log
-stdout_path      log_file
-preload_app      true
+working_directory @dir
 
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
+timeout 30
 
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
-end
+# Specify path to socket unicorn listens to, 
+# we will use this in our nginx.conf later
+listen "/home/petrovich_demo/shared/sockets/unicorn.sock", :backlog => 64
 
-before_fork do |server, worker|
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
+# Set process id path
+pid "/home/petrovich_demo/shared/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "/home/petrovich_demo/shared/log/unicorn.stderr.log"
+stdout_path "/home/petrovich_demo/shared/log/unicorn.stdout.log"
